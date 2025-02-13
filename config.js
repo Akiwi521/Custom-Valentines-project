9a9db1b8d014a79f5e4ad498301054a62411a8c0
Akiwi521 <arselem521@gmail.com>
2025-02-13 11:55:02 -0600

    4thmusicfix

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ const CONFIG = {
     // Animation settings
     // Adjust these if you want faster/slower animations
     animations: {
-        floatDuration: "20s",           // How long it takes hearts to float up (10-20s recommended)
+        floatDuration: "17s",           // How long it takes hearts to float up (10-20s recommended)
         floatDistance: "65px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "1s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.7         // Size of heart explosion effect (1.2-2.0 recommended)
@@ -77,10 +77,9 @@ const CONFIG = {
     // Add your own music URL after getting proper licenses
     music: {
         enabled: true,                     // Music feature is enabled
-        autoplay: false,                    // Try to autoplay (note: some browsers may block this)
+        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dp0ma3kde/video/upload/v1739465913/x3sfl7zq6qld9skqqo3b.mp3", // Music streaming URL
-        startText: "🎵 Play",        // Button text to start music
-        stopText: "🔇 Stop Music",         // Button text to stop music
+        startText: "🎵 Play",        // Button text to start music 
         volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
